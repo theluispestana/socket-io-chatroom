29c1ed2caee07d0c5fa7e94ac0f02cf140e3d4e8
theluispestana <theluispestana@gmail.com>
2020-09-28 20:25:11 -0400

    users can connect to chat room

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+http.listen(3000, () => {
+  console.log("listening on *: 3000");
+});
